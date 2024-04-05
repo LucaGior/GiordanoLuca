59ee0c8df4ec5aa7040e2591f06a6ad90297be1a
LucaGior <luca_gior@hotmail.com>
2024-04-05 19:37:20 -0300

    hice un for para recorrer un array

---
+++ TP1/fn.js
@@ -0,0 +1,6 @@
+array = ['pera','mazana','frutilla','banana','naranja'];
+//console.log(Array[1]);
+
+for (i = 0; i<array.length; i++) {
+    console.log(array[i]);
+}
---
+++ primeraClase/js/funciones.js
@@ -0,0 +1,11 @@
+function saludar(num) { 
+    const aux = 70;
+    if (num > aux){
+        alert("el numero es mayor que 70");
+    }   
+    else if (num < aux){
+        alert("el numero es menor que 70");
+    } 
+    else
+        alert("es igual a 70")
+}
