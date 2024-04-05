f4c8399cd270513e6c9ce0f141138bbf6e425335
LucaGior <luca_gior@hotmail.com>
2024-04-04 21:13:03 -0300

    boton con comparacion de dos valores y devolucion

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,10 @@
-function saludar(nom,a,b) { 
-    alert(nom+"el resultado es: "+(a+b));
+function saludar(num) { 
+    const aux = 70;
+    if (num > aux){
+        alert("el numero es mayor que 70")
+    }
+    else if (num < aux){
+        alert("el numero es menor que 70")
+    }
+    else alert("es igual a 70")
 }
